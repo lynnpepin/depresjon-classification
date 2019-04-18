20d9232a070fb3f2aa3917680272d3e23e55d3d0
Lynn Pepin <tpepin96@gmail.com>
2019-04-18 14:46:41 -0400

    preprocess.py truncates data to npy.
    TODO: Normalize, create single X file and y (targets) file.

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@
 # Time series preproc:
 #   Data measurements are difficult-to-work-with variable-lengths,
 #   ranging from about 19000 samples to about 65000 samples.
-#   Let's cut data into 3000-sized samples, discard extraneous data.
+#   Let's cut data into 2000-sized samples, discard extraneous data.
 #   Then, normalize via 0-1 normalization
 #       Consider: max of values across ALL participants, or max per participant?
 #                 Different people might have different levels of activity, etc.
@@ -22,6 +22,32 @@
 #   (Time series x (Norm all vs Norm per), Meta Data) x (Linear x 1-hidden),
 #   and Time series x (Neural ODE, GRU/RNN/LSTM)
 
-# todo: Actual documentation, actual preprocessing
+# todo: Write actual documentation
 
-path = "./depresjon-dataset/"
+
+import numpy as np
+import pandas as pd
+
+# We cut each time-series into parts, length 4000
+#   Result: More samples, smaller and uniform size in each sample
+TS_LENGTH = 2000
+
+condition_path = "./depresjon-dataset/condition/condition_{}.csv"
+control_path = "./depresjon-dataset/control/control_{}.csv"
+
+condition_raw = [np.array(pd.read_csv(condition_path.format(x))['activity']) for x in range(1,24)]
+control_raw = [np.array(pd.read_csv(control_path.format(x))['activity']) for x in range(1,33)]
+
+# truncate returns a series, truncated such that axis 0 has an integer multiple of TS_LENGTH
+# e.g. a.shape=(51611,32); truncate(a).shape=(50000,32)
+truncate = lambda series : series[:TS_LENGTH * (len(series) // TS_LENGTH)]
+
+# condition, control are both np_arrays shape (number of samples, TS_LENGTH)
+condition = np.concatenate([truncate(series).reshape(-1, TS_LENGTH) for series in condition_raw])
+control = np.concatenate([truncate(series).reshape(-1, TS_LENGTH) for series in control_raw])
+
+# TODO - Write normalization functions!
+
+np.save("condition_{}".format(TS_LENGTH), condition)
+np.save("control_{}".format(TS_LENGTH), control)
+
