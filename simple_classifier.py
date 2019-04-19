57217ad7515077628f1dec8a02774406a1725c3c
Lynn Pepin <tpepin96@gmail.com>
2019-04-19 08:52:10 -0400

    Made a simple classifier in Keras for the data

---
+++ simple_classifier.py
@@ -0,0 +1,55 @@
+"""
+Build a simple 1-layer classifier using Keras.
+"""
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Activation, Input, GRU
+from keras.optimizers import RMSprop, Adam
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import confusion_matrix
+from keras.utils import to_categorical
+from time import time
+
+TS_LENGTH = 2000
+TRAINSPLIT = 652/752
+VALSPLIT   = 100/652
+RANDOMSTATE= 413
+
+condition = np.load("condition_{}.npy".format(TS_LENGTH))
+control = np.load("control_{}.npy".format(TS_LENGTH))
+
+X = np.concatenate((condition, control), axis=0)
+y = to_categorical(np.array([0]*len(condition) + [1]*len(control)))
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 1-TRAINSPLIT,
+                                                    random_state = RANDOMSTATE)
+
+X_train, X_val, y_train, y_val   = train_test_split(X_train, y_train,
+                                                    test_size=VALSPLIT,
+                                                    random_state = RANDOMSTATE)
+
+
+model = Sequential()
+#model.add(Dense(4096, input_dim = TS_LENGTH, activation = 'relu'))
+#model.add(Dense(2, activation = 'softmax'))
+model.add(Dense(2, input_dim = TS_LENGTH, activation = 'softmax'))
+
+model.compile(loss='binary_crossentropy',
+     optimizer=Adam(),
+     metrics=['accuracy'])
+
+history = model.fit(X_train, y_train,
+                    batch_size = 32,
+                    epochs = 50,
+                    callbacks=[EarlyStopping(patience=10)],
+                    validation_split = 0.1)
+
+# TODO: More in depth analysis
+yh = model.predict(X_val)
+ypred = np.argmax(yh, axis=1)
+ytrue = y_val[:,1]
+print("Accuracy:", np.sum(ypred == ytrue)/100)
