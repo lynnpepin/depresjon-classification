5b6073668d8c65d54104c1c7fcd4359f7a412f3d
Lynn Pepin <tpepin96@gmail.com>
2019-04-25 15:15:23 -0400

    yapf.now.sh auto pep8 format

--- simple_classifier_pytorch.py
+++ simple_classifier_pytorch.py
@@ -3,6 +3,7 @@ Build a simple 1-layer classifier using Pytorch.
 # https://github.com/andrewliao11/dni.pytorch/blob/master/mlp.py
 """
 
+from pathlib import Path
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
@@ -15,12 +16,11 @@ import torch.optim as optim
 import torch.utils.data
 from torch.utils.data.dataset import Dataset
 from torch.autograd import Variable
-from pathlib import Path
 
 TS_LENGTH = 2000
-TRAINSPLIT = 652/752
-VALSPLIT   = 100/652
-RANDOMSTATE= 413
+TRAINSPLIT = 652 / 752
+VALSPLIT = 100 / 652
+RANDOMSTATE = 413
 
 NUM_EPOCHS = 100
 BATCH_SIZE = 32
@@ -33,51 +33,53 @@ np.random.seed(RANDOMSTATE)
 condition = np.load(Path("./condition_{}_emb.npy".format(TS_LENGTH)))
 control = np.load(Path("./control_{}_emb.npy".format(TS_LENGTH)))
 X = np.concatenate((condition, control), axis=0)
-y = np.array([0]*len(condition) + [1]*len(control))
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size = 1-TRAINSPLIT, random_state = RANDOMSTATE)
+y = np.array([0] * len(condition) + [1] * len(control))
+train_X, test_X, train_y, test_y = train_test_split(
+    X, y, test_size=1 - TRAINSPLIT, random_state=RANDOMSTATE)
+
 
 # The Dataset class required by PyTorch
 class GenericDataset(Dataset):
     def __init__(self, X, y):
         self.X = X
         self.y = y
-    
+
     def __getitem__(self, index):
         return (self.X[index], self.y[index])
-    
+
     def __len__(self):
-        assert(len(self.X) == len(self.y))
+        assert (len(self.X) == len(self.y))
         return len(self.X)
+
 
 train_dataset = GenericDataset(train_X, train_y)
 test_dataset = GenericDataset(test_X, test_y)
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                     batch_size=BATCH_SIZE,
-                                     shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                     batch_size=BATCH_SIZE,
-                                     shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 
 # Define our NN:
 class Net(nn.Module):
-    def __init__(self, input_size = 2, hidden_size = 4, num_classes = 2):
+    def __init__(self, input_size=2, hidden_size=4, num_classes=2):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
-    
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
         return out
 
+
 net = Net()
 # Loss and Optimizer
-criterion = nn.CrossEntropyLoss()  
+criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters())
 
 # Time to Train
@@ -90,19 +92,24 @@ for epoch in range(NUM_EPOCHS):
         loss = criterion(outputs, Variable(y))
         loss.backward()
         optimizer.step()
-        
+
     if (epoch + 1) % 10 == 0:
-        print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' 
-               %(epoch+1, NUM_EPOCHS, i+1, len(train_dataset)//BATCH_SIZE, loss.data))
+        print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' %
+              (epoch + 1, NUM_EPOCHS, i + 1, len(train_dataset) // BATCH_SIZE,
+               loss.data))
 
 # Save the Model
 torch.save(net.state_dict(), 'model.pkl')
 
 predict_out = torch.Tensor(test_X)
 _, predict_y = torch.max(predict_out, 1)
-print( 'prediction accuracy', accuracy_score(test_y.data, predict_y.data))
-print( 'macro precision', precision_score(test_y.data, predict_y.data, average='macro'))
-print( 'micro precision', precision_score(test_y.data, predict_y.data, average='micro'))
-print( 'macro recall', recall_score(test_y.data, predict_y.data, average='macro'))
-print( 'micro recall', recall_score(test_y.data, predict_y.data, average='micro'))
+print('prediction accuracy', accuracy_score(test_y.data, predict_y.data))
+print('macro precision',
+      precision_score(test_y.data, predict_y.data, average='macro'))
+print('micro precision',
+      precision_score(test_y.data, predict_y.data, average='micro'))
+print('macro recall', recall_score(
+    test_y.data, predict_y.data, average='macro'))
+print('micro recall', recall_score(
+    test_y.data, predict_y.data, average='micro'))
 
